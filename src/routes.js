2919535ced88743e202899cbbb955257a7273f8b
Colin Heinzmann <colin@jut.io>
2014-06-25 16:13:39 -0700

    no more info page

--- src/routes.js
+++ src/routes.js
@@ -5,18 +5,6 @@ var router = express.Router();
 // require the itunes library
 var itx_import = require("./util/itunes-xml-import");
 
-/* GET home page. */
-router.get('/info', function(req, res) {
-	res.render('index', {
-		request: req.path,
-		title: 'NodeJS Demo Application',
-		hostname: os.hostname(),
-		platform: os.platform(),
-		memused: (os.totalmem() - os.freemem()) / 1000000,
-		memusage: (os.totalmem() - os.freemem()) / os.totalmem()
-	});
-});
-
 router.get("/Music*", function(req, res) {
 	// open the file we requested
 	musicRoot = "/home/colin/temp/iTunes/iTunes Media";
