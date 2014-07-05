e15a2af0cdfda0db6bbd35d18c4ce7a78910420f
DepthDeluxe <cheinzmann3@gmail.com>
2014-07-05 15:02:33 -0700

    added designs for playlist querying

--- src/designs.js
+++ src/designs.js
@@ -2,23 +2,49 @@ var nano = require('nano');
 var seed = require('couchdb-seed-design');
 var db = nano('http://localhost:5984').use('itunes-web');
 
+var trackMatch = /track[0-9]+/;
+var playlistMatch = /playlist[0-9]+/;
+
 seed(db, {
     tracks: {
         views: {
-            allTracks: function(doc) {
-                emit(doc);
-            },
             allIds: function(doc) {
-                emit(doc.id, doc.name);
+                if (doc._id.match(trackMatch)) {
+                    emit(doc.id, doc.name);
+                }
             },
             byId: function(doc) {
-                emit(doc.id, doc);
+                if (doc._id.match(trackMatch)) {
+                    emit(doc.id, doc);
+                }
             },
             byName: function(doc) {
-                emit(doc.name, doc);
+                if (doc._id.match(trackMatch)) {
+                    emit(doc.name, doc);
+                }
+            }
+        }
+    },
+
+    playlists: {
+        views: {
+            allIds: function(doc) {
+                if (doc._id.match(playlistMatch)) {
+                    emit(doc.id, doc.name);
+                }
+            },
+            byId: function(doc) {
+                if (doc._id.match(playlistMatch)) {
+                    emit(doc.id, doc);
+                }
+            },
+            byName: function(doc) {
+                if (doc._id.match(playlistMatch)) {
+                    emit(doc.name, doc);
+                }
             }
         }
     }
 }, function() {
-    console.log('loaded the track documents');
+    console.log('loaded the design documents');
 });
